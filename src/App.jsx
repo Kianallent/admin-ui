20e4e1cae110aec88498877b2a2a84d57446116f
Kianallent <allentabhima05@gmail.com>
2025-10-14 19:25:08 +0700

    menggunakan variant class tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const courses = [
+    {
+      title: "System Administration and IT Infrastructure Services",
+    },
+    {
+      title: "Operating Systems Becoming a Power User",
+    },
+    {
+      title: "The Bits and Bytes of Computer Networking",
+    },
+    {
+      title: "Technical Support Fundamentals",
+    },
+    {
+      title: "How to Succeed at: Writing Applications",
+    },
+    {
+      title: "Medicine Administration for Carers",
+    },
+  ];
 
   return (
     <>
-      <div className="min-h-screen bg-neutral-800 flex flex-col items-center justify-center text-center px-4">
-        {/* Login Card */}
-        <div className="grid grid-cols-2 w-150 h-125 bg-white rounded-xl shadow-lg overflow-hidden">
-          {/* Left Side */}
-          <div className="flex items-center justify-center bg-gray-200 text-gray-600 text-3xl font-bold">
-            600 × 500
-          </div>
+      <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
+        {courses.map((course, index) => (
+          <div key={index} className="shadow rounded-lg overflow-hidden flex flex-col 
+          hover:border hover:border-gray-500
+          hover:shadow-lg hover:scale-105 transition-transform duration-300">
+            {/* Image */}
+            <img
+              src="https://www.placehold.co/300x200"
+              alt={course.title}
+              className="w-full h-44 object-cover"
+            />
 
-          {/* Right Side */}
-          <div className="flex items-center justify-center p-10">
-            <form className="w-full">
-              <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">Login</h2>
+            {/* Content */}
+            <div className="p-4 bg-red-100 flex flex-col justify-between h-full">
+              <h3 className="text-lg font-semibold mb-4">{course.title}</h3>
 
-              {/* Email Input */}
-              <div className="mb-5">
-                <label className="block mb-2 text-sm font-medium text-gray-700 text-left">Email</label>
-                <input
-                  type="email"
-                  placeholder="Masukkan email"
-                  className="w-full px-3 py-2 border border-gray-300 rounded-lg 
-                             focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                />
+              <div className="bg-red-50 p-2 rounded-lg">
+                <div className="text-sm text-gray-500 mt-2 flex justify-between">
+                  <span>👥 123 users</span>
+                  <span>⏱ 60 min</span>
+                </div>
+
+                {/* Author */}
+                <div className="mt-3 mb-2 flex items-center gap-2">
+                  <img
+                    src="https://www.placehold.co/50x50"
+                    alt="Author's Avatar"
+                    className="w-12 h-12 rounded-full object-cover"
+                  />
+                  <div>
+                    <p className="text-sm font-medium">Author's Name</p>
+                    <p className="text-xs text-gray-500">Designer</p>
+                  </div>
+                </div>
               </div>
-
-              {/* Password Input */}
-              <div className="mb-6">
-                <label className="block mb-2 text-sm font-medium text-gray-700 text-left">Password</label>
-                <input
-                  type="password"
-                  placeholder="Masukkan password"
-                  className="w-full px-3 py-2 border border-gray-300 rounded-lg 
-                             focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                />
+              {/* Footer */}
+              <div className="mt-4 flex items-center justify-between">
+                <div className="bg-white py-2 px-4 rounded">$123</div>
+                <button className="bg-red-700 hover:bg-red-900 active:bg-red-500 text-white px-4 py-2 rounded text-sm">
+                  Get Started
+                </button>
               </div>
-
-              {/* Button */}
-              <button
-                type="submit"
-                className="w-full py-2 bg-blue-600 text-white rounded-lg 
-                           hover:bg-blue-700 transition-colors shadow"
-              >
-                Login
-              </button>
-            </form>
+            </div>
           </div>
-        </div>
-        {/* End Login Card */}
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
